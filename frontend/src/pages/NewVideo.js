a0281c408dd40967cb9308c8151737ad8cfb2bb8
yihsuan <yiiihsuann@gmail.com>
2024-07-30 16:08:51 +0800

    test download

--- frontend/src/pages/NewVideo.js
+++ frontend/src/pages/NewVideo.js
@@ -221,7 +221,8 @@ const NewVideo = ({  onNextPage, activeIndex, steps, videoUrl }) => {
   // };
 
   const handleDownloadAndNext = () => {
-    fetch(videoUrl, { mode: 'no-cors' })
+    fetch(videoUrl)
+    // fetch(videoUrl, { mode: 'no-cors' })
     .then(response => response.blob())  // 將response 轉換為Blob
     .then(blob => {
       const url = window.URL.createObjectURL(blob);  // 創建一個指向 Blob 的 URL
