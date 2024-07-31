6dc25d486a285e77045a87050ba5ca2550e8d399
yihsuan <yiiihsuann@gmail.com>
2024-07-31 16:01:31 +0800

    test character card

--- frontend/src/component/CharacterCard.js
+++ frontend/src/component/CharacterCard.js
@@ -106,14 +106,12 @@ const Card = styled.div`
   border-bottom: 5px solid #000;
   border-left: 1px solid #000;
   margin: 10px;
-  padding:10px;
-  padding-bottom: 20px;
 `;
 
 const CharacterImage = styled.img`
-  width: 100%;
+  //width: 100%;
   height:60%;
-  max-height: 300px;
+  max-height: 200px;
   object-fit: cover;
   border-bottom: 1px solid #000;
 `;
@@ -122,24 +120,24 @@ const CharacterInfo = styled.div`
   flex-grow: 1;
   display: flex;
   flex-direction: column;
-  justify-content: space-between;
+  justify-content: flex-start;
   padding: 1rem;
   text-align: center;
 `;
 
 const CharacterName = styled.h3`
-  font-size: 24px;
-  margin: 5px 0;
+  font-size: 1.5rem;
+  margin: 2px 0;
 `;
 
 const CharacterDescription = styled.p`
   font-size: 1rem;
-   margin: 5px 0;
+  margin: 3px 0;
 `;
 
 const AudioPlayer = styled.audio`
   width: 100%;
-  margin: 10px auto 20px; 
+  margin: 10px auto;
   display: block;
 `;
 
